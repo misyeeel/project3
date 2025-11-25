cdc852b3b2ba07a59ae9a9e208a9775f820d7521
michelle <misyeeelolfindo@gmail.com>
2025-11-26 00:14:09 +0800

    Add driver.js

---
+++ assets/drivers.js
@@ -0,0 +1,122 @@
+var drivers = [
+    {
+        "driverName": "PIERRE GASLY",
+        "driverTeam": "Alphine",
+        "driverNo": 10,
+        "image": "assets/imgvid/driver1-Pierre Gasly.jpg"
+    },
+    {
+        "driverName": "FRANCO COLAPINTO",
+        "driverTeam": "Alphine",
+        "driverNo": 43,
+        "image": "assets/imgvid/driver2-Franco Colapinto.jpg"
+    },    
+    {
+        "driverName": "LANCE STROLL",
+        "driverTeam": "Aston Martin",
+        "driverNo": 18,
+        "image": "assets/imgvid/driver3-Lance Stroll.jpg"
+    },
+    {
+        "driverName": "FERNANDO ALONSO",
+        "driverTeam": "Aston Martin",
+        "driverNo": 14,
+        "image": "assets/imgvid/driver4-Fernando Alonso.jpg"
+    },
+    {
+        "driverName": "CHARLES LECLERC",
+        "driverTeam": "Ferrari",
+        "driverNo": 16,
+        "image": "assets/imgvid/driver5-Charles Leclerc.jpg"
+    },
+    {
+        "driverName": "LEWIS HAMILTON",
+        "driverTeam": "Ferrari",
+        "driverNo": 44,
+        "image": "assets/imgvid/driver6-Lewis Hamilton.jpg"
+    },
+    {
+        "driverName": "ESTEBAN OCON",
+        "driverTeam": "Haas F1 Team",
+        "driverNo": 31,
+        "image": "assets/imgvid/driver7-Esteban Ocon.jpg"
+    },
+    {
+        "driverName": "OLIVER BEARMAN",
+        "driverTeam": "Haas F1 Team",
+        "driverNo": 87,
+        "image": "assets/imgvid/driver8-Oliver Bearman.jpg"
+    },
+    {
+        "driverName": "NICO HULKENBERG",
+        "driverTeam": "Kick Sauber",
+        "driverNo": 27,
+        "image": "assets/imgvid/driver9-Nico Hulkenberg.jpg"
+    },
+    {
+        "driverName": "GABRIEL BORTOLETO",
+        "driverTeam": "Kick Sauber",
+        "driverNo": 5,
+        "image": "assets/imgvid/driver10-Gabriel Bortoleto.jpg"
+    },
+    {
+        "driverName": "OSCAR PIASTRI",
+        "driverTeam": "McLaren",
+        "driverNo": 81,
+        "image": "assets/imgvid/driver11-Oscar Piastri.jpg"
+    },
+    {
+        "driverName": "LANDO NORRIS",
+        "driverTeam": "McLaren",
+        "driverNo": 4,
+        "image": "assets/imgvid/driver12-Lando Norris.jpg"
+    },
+    {
+        "driverName": "GEORGE RUSSELL",
+        "driverTeam": "Mercedes",
+        "driverNo": 63,
+        "image": "assets/imgvid/driver13-George Russell.jpg"
+    },
+    {
+        "driverName": "KIMI ANTONELLI",
+        "driverTeam": "Mercedes",
+        "driverNo": 12,
+        "image": "assets/imgvid/driver14-Kimi Antonelli.jpg"
+    },
+    {
+        "driverName": "LIAM LAWSON",
+        "driverTeam": "Racing Bulls",
+        "driverNo": 30,
+        "image": "assets/imgvid/driver15-Liam Lawson.jpg"
+    },
+    {
+        "driverName": "ISACK HADJAR",
+        "driverTeam": "Racing Bulls",
+        "driverNo": 6,
+        "image": "assets/imgvid/driver16-Isack Jadjar.jpg"
+    },
+    {
+        "driverName": "MAX VERSTAPPEN",
+        "driverTeam": "Red Bull Racing",
+        "driverNo": 1,
+        "image": "assets/imgvid/driver17-Max Verstappen.jpg"
+    },
+    {
+        "driverName": "YUKI TSUNODA",
+        "driverTeam": "Red Bull Racing",
+        "driverNo": 22,
+        "image": "assets/imgvid/driver18-Yuki Tsunoda.jpg"
+    },
+    {
+        "driverName": "ALEXANDER ALBON",
+        "driverTeam": "Williams",
+        "driverNo": 23,
+        "image": "assets/imgvid/driver19-Alexander Albon.jpg"
+    },
+    {
+        "driverName": "CARLOS SAINZ",
+        "driverTeam": "Williams",
+        "driverNo": 55,
+        "image": "assets/imgvid/driver20-Carlos Sainz.jpg"
+    }
+];
