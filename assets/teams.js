dd3422b2219edc862bf390a577bc4fc340b91ef4
michelle <misyeeelolfindo@gmail.com>
2025-11-26 00:17:53 +0800

    Update javascript

--- assets/teams.js
+++ assets/teams.js
@@ -4,55 +4,46 @@ var teams = [
         "teamChief": "EnsFlavio Briatore",
         "image": "assets/imgvid/team1.jpg"
     },
-
     {
         "teamName": "ASTON MARTIN",
         "teamChief": "Andy Cowell",
         "image": "assets/imgvid/team2.jpg"
     },
-
     {
         "teamName": "FERRARI",
         "teamChief": "Frédéric Vasseur",
         "image": "assets/imgvid/team3.jpg"
     },
-
     {
         "teamName": "HAAS F1 TEAM",
         "teamChief": "Ayao Komatsu",
         "image": "assets/imgvid/team4.jpg"
     },
-
     {
         "teamName": "KICK SAUBER",
         "teamChief": "Jonathan Wheatley",
         "image": "assets/imgvid/team5.jpg"
     },
-
     {
         "teamName": "MC LAREN",
         "teamChief": "Andrea Stella",
         "image": "assets/imgvid/team6.jpg"
     },
-
     {
         "teamName": "MERCEDES",
         "teamChief": "Toto Wolff",
         "image": "assets/imgvid/team7.jpg"
     },
-
     {
         "teamName": "RACING BULLS",
         "teamChief": "Alan Permane",
         "image": "assets/imgvid/team8.jpg"
     },
-
     {
         "teamName": "RED BULL RACING",
         "teamChief": "Laurent Mekies",
         "image": "assets/imgvid/team9.jpg"
     },
-
     {
         "teamName": "WILLIAMS",
         "teamChief": "James Vowles",
