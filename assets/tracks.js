56deb71d49863c771467d9e5c540f73d85fd8cab
michelle <misyeeelolfindo@gmail.com>
2025-11-26 00:15:20 +0800

    Add tracks.js

---
+++ assets/tracks.js
@@ -0,0 +1,27 @@
+var tracks = [
+    {
+        "trackName": "AUSTRALIAN GRAND PRIX",
+        "trackLocation": "Albert Park",
+        "image": "assets/imgvid/track1.jpg"
+    },
+    {
+        "trackName": "JAPANESE GRAND PRIX",
+        "trackLocation": "Suzuka Circuit",
+        "image": "assets/imgvid/track2.jpg"
+    },
+    {
+        "trackName": "MONACO GRAND PRIX",
+        "trackLocation": "Circuit De Monaco",
+        "image": "assets/imgvid/track3.jpg"
+    },
+    {
+        "trackName": "SPANISH GRAND PRIX",
+        "trackLocation": "Circuit De Catalunya",
+        "image": "assets/imgvid/track4.jpg"
+    },
+    {
+        "trackName": "SINGAPORE GRAND PRIX",
+        "trackLocation": "Marina Bay Street Circuit",
+        "image": "assets/imgvid/track5.jpg"
+    }
+];
